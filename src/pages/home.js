35fc6d5f894c000fc4220730fb3e0217a373a415
Max Gomes <maxgomes92@live.com>
2020-04-05 13:57:46 +0200

    Add home content

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import Header from '../components/Header';
+import './home.css'
 
 function Home () {
   return (
-    <Header />
+    <div className='Home'>
+      <Header />
+      <div className='top'>
+        <div className='wrapper'>
+          <h1 className='prefix'>Help is</h1>
+          <h1 className='suffix'>Blind</h1>
+        </div>
+      </div>
+      <div className='mid'>
+        <div className='wrapper'>
+          <h1>How does it work?</h1>
+          <h1 style={{fontSize: 100}}>Place holder animation?</h1>
+        </div>
+      </div> 
+      <div className='bottom'>
+        <div className='wrapper'>
+          <h1 style={{fontSize: 100}}>Other ways to go</h1>
+        </div>
+      </div>            
+    </div>
   )
 }
 
